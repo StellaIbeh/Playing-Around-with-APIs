98af09b28439a1f0ce43bb66538d2352dd08430a
StellaIbeh <a.ibeh@alustudent.com>
2023-08-18 20:18:45 +0200

    updated README.md file

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
 		"x-rapidapi-host": "quotes15.p.rapidapi.com",
 		"x-rapidapi-key": "9bee5666b1msh139212b7851e3fep1279fcjsn10a28f5463bf"
 	}
-})
+}
+)
 .then(response => response.json())
 .then(response => {
     console.log(response);
